eac9e5275f8b4926ad540fe1c5341d0e53091073
Raj Wadhwa <30954386+raj-wadhwa@users.noreply.github.com>
2017-11-16 20:00:49 +0530

    Add files via upload
    New Changes...Stored Procedure for Checking User and few other changes

--- myProj/sp_checkUser.sql
+++ myProj/sp_checkUser.sql
@@ -2,10 +2,11 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_checkUser`(
 	IN p_username VARCHAR(25),
     IN p_password VARCHAR(200))
 BEGIN
-	IF (Select exists (Select COUNT(*) from Customer where username=p_username
-		and password=p_password)) then
-		select 'Logged-In';
-	ELSE
-		select 'Username or Password is incorrect. If you don\'t have an account, Please Sign Up !';
-  END IF;
+	declare C INT;
+	Select COUNT(*) INTO C from Customer where username=p_username
+		and password=p_password;
+    IF(C != 1) THEN 
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Username or Password is Wrong! If you are a new user, Please Sign Up.';
+	END IF;
 END
